4039e8090e14e13cc548b8dd83c92cf4327dfc47
amit-k-nain <amitknain146@gmail.com>
2020-06-20 01:56:22 +0530

    modify code

--- upload/admin/language/english/catalog/card.php
+++ upload/admin/language/english/catalog/card.php
@@ -43,7 +43,6 @@ $_['entry_sort_order']       = 'Sort Order:';
 $_['entry_layout']           = 'Layout Override:';
 $_['entry_profile']          = 'Profile:';
 
-
 // Error
 $_['error_warning']          = 'Warning: Please check the form carefully for errors!';
 $_['error_permission']       = 'Warning: You do not have permission to modify card!';
